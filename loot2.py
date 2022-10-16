8b5effa9bc4a739a1a147efdfdd1bc7bc281f26c
henrymuddleton <85002333+henrymuddleton@users.noreply.github.com>
2022-10-16 15:53:48 -0700

    Update loot2.py

--- loot2.py
+++ loot2.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from pyvirtualdisplay import Display
-from webdriver_manager.chrome import ChromeDriverManager
 
 email = 'logjefferson6@gmail.com'
 password = 'xonB23Y5'
+path='/content/loot/chromedriver.exe'
 
 
 if __name__ == '__main__':
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     chrome_options = Options()
     chrome_options.add_argument('--disable-notifications')
     chrome_options.add_extension('tamper.crx')
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver = webdriver.Chrome(executable_path=path,options=chrome_options)
 
     ## install focus
     # go to install page
